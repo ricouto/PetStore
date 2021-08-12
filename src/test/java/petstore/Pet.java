b303f1ddf4429377feaa9a9bd2d8bec74fdb6a1b
Ricardo Couto <ricardo.couto@wolterskluwer.com>
2021-08-12 20:29:18 -0300

    validacao do body name, status e category.name

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 public class Pet {
     String uri = "https://petstore.swagger.io/v2/pet";
@@ -28,6 +29,9 @@ public class Pet {
         .then()
                 .log().all()
                 .statusCode(200)
+                .body("name", is("camilinha") )
+                .body("status", is("available"))
+                .body("category.name", is("cachorro"))
         ;
 
     }
