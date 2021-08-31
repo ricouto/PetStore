daa06322df809f7aeb8bf5359402c0b22c458942
Ricardo Couto <ricardo.couto@wolterskluwer.com>
2021-08-30 21:00:03 -0300

    finalizada a Suite Store

--- src/test/java/petstore/Store.java
+++ src/test/java/petstore/Store.java
@@ -1,17 +1,28 @@
 package petstore;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.Data;
+
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 public class Store {
-
+    Data data;
+    String orderId;
     String getStore = "store/inventory";
+    String verboGetPostDelete = "store/order";
     String uri = "https://petstore.swagger.io/v2";
 
+    @BeforeMethod
+    public void setUp(){
+        data = new Data();
+    }
+
     @Test(priority = 1)
-    public void consultarStore(){
+    public void test1_consultarStoreInventory(){
 
         given()
                 .contentType("application/json")
@@ -24,8 +35,58 @@ public class Store {
                 //.body("id", equalTo(petId02))
                 //.body("sold", is(2))
         ;
-
     }
 
+    @Test(priority = 2)
+    public void test2_incluirStoreOrder() throws IOException {
+        String jsonBody = data.lerJson("db/store1.json");
+
+        orderId = given()
+                .contentType("application/json")
+                .body(jsonBody)
+        .when()
+                .post(uri + "/" + verboGetPostDelete)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(9))
+                .body("petId", is(7))
+                .body("quantity", is(1))
+                .extract().path("id").toString()
+        ;
+    }
+
+    @Test(priority = 3)
+    public void test3_consultarStoreOrderID(){
+
+        given()
+                .contentType("application/json")
+                //.log().all()
+        .when()
+                .get(uri + "/" + verboGetPostDelete+ "/" + orderId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(9))
+                .body("petId", is(7))
+                .body("quantity", is(1))
+        ;
+    }
+
+    @Test(priority = 4)
+    public void test4_deleteOrderId() throws IOException {
+
+        given()
+                .contentType("application/json")
+        .when()
+                .delete(uri + "/" + verboGetPostDelete+ "/" + orderId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(orderId))
+        ;
+    }
 
 }
